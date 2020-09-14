276ee69e8d197e921821b60384df5c2c51c5030d
al-amin-khan <alaminkhan2010@gmail.com>
2020-09-14 14:14:41 +0600

    added tests

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PostListView
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post'),
-    
+    path('', PostListView.as_view(), name='home'),
+
 ]
